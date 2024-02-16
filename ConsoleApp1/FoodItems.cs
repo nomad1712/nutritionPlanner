797f8103359e2e3b56479dbf74a71f1e0ff80a6e
nomad1712 <123312290+nomad1712@users.noreply.github.com>
2024-02-15 20:45:35 -0500

    Add files via upload

---
+++ ConsoleApp1/FoodItems.cs
@@ -0,0 +1,29 @@
+public class FoodItem<F>
+{
+    // Properties to store basic nutrition information
+    public string Name { get; set; }
+    public double Fat { get; set; } // in grams
+    public double Protein { get; set; } // in grams
+    public double Sugar { get; set; } // in grams
+    public double Sodium { get; set; } // in milligrams
+
+    // Constructor
+    public FoodItem(string name, double fat, double protein, double sugar, double sodium)
+    {
+        Name = name;
+        Fat = fat;
+        Protein = protein;
+        Sugar = sugar;
+        Sodium = sodium;
+    }
+
+    // Method to display information about the food item
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Fat: {Fat}g");
+        Console.WriteLine($"Protein: {Protein}g");
+        Console.WriteLine($"Sugar: {Sugar}g");
+        Console.WriteLine($"Sodium: {Sodium}mg");
+    }
+}
